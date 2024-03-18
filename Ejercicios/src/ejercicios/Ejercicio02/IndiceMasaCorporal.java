30927b27affd54324f9f9e39f9747263a76eee33
DevCenDev <centurionllatasalexdaniel@gmail.com>
2024-03-18 01:58:11 -0500

    ejercicio02 resuelto

--- Ejercicios/src/ejercicios/Ejercicio02/IndiceMasaCorporal.java
+++ Ejercicios/src/ejercicios/Ejercicio02/IndiceMasaCorporal.java
@@ -1,8 +1,28 @@
 
 package ejercicios.Ejercicio02;
 
+import java.util.Scanner;
+
 public class IndiceMasaCorporal {
     public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        
+        float peso, altura;
+        
+        System.out.print("Ingrese su peso en Kg: ");
+        peso = lectura.nextFloat();
+        
+        System.out.print("Ingrese la altura en m: ");
+        altura = lectura.nextFloat();
+        
+        // Se usa la clase math para elevar al cuadrado la variable altura
+        
+        float imc = (float) (peso / Math.pow(altura, 2));
+        
+        System.out.printf("""
+                          El indice de masa corporal es: %.2f
+                          """, imc);
+        
         
     }
 
